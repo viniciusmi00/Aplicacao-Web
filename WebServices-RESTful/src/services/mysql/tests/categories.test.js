7d507b834e533abe616a38228e2b3bf017d5a31b
Vinicius P. Miranda <viniciusmi00@hotmail.com>
2019-06-07 15:25:08 -0300

    Testes Categoria

--- WebServices-RESTful/src/services/mysql/tests/categories.test.js
+++ WebServices-RESTful/src/services/mysql/tests/categories.test.js
@@ -5,8 +5,8 @@ const categories = require('../categories')(dependencies)
 
 const create = () => categories.save('category-test')
 
-//test.beforeEach (t => connection.query('TRUNCATE TABLE categories')) // Limpar a tabela antes da execução dos testes
-//test.after.always (t => connection.query('TRUNCATE TABLE categories')) // Limpar a tabela depois da execução dos testes. always garante que execute mesmo que o teste falhe.
+test.beforeEach (t => connection.query('TRUNCATE TABLE categories')) // Limpar a tabela antes da execução dos testes
+test.after.always (t => connection.query('TRUNCATE TABLE categories')) // Limpar a tabela depois da execução dos testes. always garante que execute mesmo que o teste falhe.
 
 test.serial('Lista de categoria', async t => {
     await create()
